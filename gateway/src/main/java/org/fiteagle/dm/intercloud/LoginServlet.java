4227d4e6196000bcaf18d4fc00acac20de0d7a83
XGerade <billylinux.oi@gmail.com>
2014-06-23 13:04:41 +0200

    Web interface files added

---
+++ gateway/src/main/java/org/fiteagle/dm/intercloud/LoginServlet.java
@@ -0,0 +1,51 @@
+package org.fiteagle.dm.intercloud;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
+import org.jivesoftware.smack.XMPPException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	public LoginServlet() {
+		super();
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+    	Gateway gateway = new Gateway("localhost", 5222);
+    	try {
+    		gateway.init();
+    	} catch (XMPPException e) {
+    		e.printStackTrace();
+    	}
+       if(gateway.performLogin(user, pwd)){
+            HttpSession session = request.getSession();
+            session.setAttribute("gateway", gateway);
+            session.setAttribute("user", user);
+            //setting session to expiry in 30 mins
+            session.setMaxInactiveInterval(30*60);
+            Cookie userName = new Cookie("user", user);
+            userName.setMaxAge(30*60);
+            response.addCookie(userName);
+            response.sendRedirect("LoginSuccess.html");
+        }else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out= response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(request, response);
+        }
+	}
+
+}
