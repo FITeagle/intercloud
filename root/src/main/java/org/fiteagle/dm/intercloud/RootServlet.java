e582e2906554b27540f7ce9cdc40e9fb3a2a479c
XGerade <billylinux.oi@gmail.com>
2014-07-09 16:42:38 +0200

    update XMPP connection fro Root

--- root/src/main/java/org/fiteagle/dm/intercloud/RootServlet.java
+++ root/src/main/java/org/fiteagle/dm/intercloud/RootServlet.java
@@ -31,7 +31,7 @@ public class RootServlet extends HttpServlet {
     public void init(ServletConfig config) {
     	
     	Root root = new Root("localhost", 5222);
-        root.init("http://localhost:3030/IaaS/"); 
+        root.init("http://root-intercloud.av.tu-berlin.de:3030/IaaS/"); 
         config.getServletContext().setAttribute("root", root);
     }
 
